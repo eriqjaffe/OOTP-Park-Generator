1dbed5ec2d7de94f64c9b7e1c938226f63c7c13f
eriqjaffe <eriqjaffe@gmail.com>
2022-03-01 18:51:36 -0600

    update electron, express port shuffle & prettier installer

--- main.js
+++ main.js
@@ -103,10 +103,8 @@ initSqlJs().then(function(SQL){
   db = new SQL.Database(filebuffer);
 });
 
-const port = 8083;
-
-app2.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const server = app2.listen(0, () => {
+	console.log(`Server running on port ${server.address().port}`);
 });
 
 app2.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
@@ -948,7 +946,7 @@ const createWindow = () => {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL(`file://${__dirname}/index.html?port=${server.address().port}`);
 
   mainWindow.webContents.on('new-window', function(e, url) {
     e.preventDefault();
